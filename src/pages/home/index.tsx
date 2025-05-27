1655349f02e904645cac0a8b7e48171ed7980048
not-meet <mjmeet.279@gmail.com>
2025-05-27 07:44:36 +0530

    initialised the reposetory

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header"
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-red-300">Home page</h1>
+      <Header />
+    </>
+  )
+}
+export default Home
